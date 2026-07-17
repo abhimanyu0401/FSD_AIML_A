0148125c6cb72b75c716b4e0ec5287a5a49f226b
Abhimanyu <209248333+abhimanyu0401@users.noreply.github.com>
2026-07-17 14:20:27 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,25 @@
+var a = 10
+var a = 20
+console.log("a = ",a);
+
+let b = 10;
+// let b =20 // not allowed
+b = 20 
+console.log("b = ",b);
+
+const c = 10;
+// const c = 34 // not allowed
+// c = 23 // not allowed
+console.log("c = ",c);
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+const sum = (a,b) => a+b;
+console.log("sum = ",sum(a,b));
+
+
+
+
